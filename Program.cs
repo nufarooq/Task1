50f5d30fee612bd2e8492f878b7a29f64a51e4f5
Tejora <numan.992@gmail.com>
2023-01-20 19:38:41 +0530

    Task 1 commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PersonTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            int total = 3;
+            Person[] persons = new Person[total];
+            Console.WriteLine("Enter 3 Person Name");
+            for (int i = 0; i < total; i++)
+            {
+                persons[i] = new Person(Console.ReadLine());
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return "Enter name is " + Name;
+        }
+
+        ~Person()
+        {
+            Name = string.Empty;
+        }
+    }
+}
